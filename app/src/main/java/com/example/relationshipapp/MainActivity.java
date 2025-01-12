0e441b807565f0fbf5687a3fd368f5435bcdfdfe
yukari1200 <tratamkhoango@gmail.com>
2025-01-12 17:04:03 +0700

    2nd

--- app/src/main/java/com/example/relationshipapp/MainActivity.java
+++ app/src/main/java/com/example/relationshipapp/MainActivity.java
@@ -1,30 +1,53 @@
 package com.example.relationshipapp;
 
+import android.app.DatePickerDialog;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.time.temporal.ChronoUnit;
+import java.time.LocalDate;
+
 public class MainActivity extends AppCompatActivity {
 
-    private DatabaseManager dbManager;
-    private Cursor cursor;
-    private final String[] from = new String[] {
-            DatabaseHelper.ID,
-            DatabaseHelper.DATE,
-            DatabaseHelper.DISPLAY_TYPE,
-    };
+    LocalDate currentDate = LocalDate.now();
+
+    private TextView relation_date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dbManager = new DatabaseManager(this);
-        //showCurrencyList();
+
+        relation_date = findViewById(R.id.displayDate);
+        Button button = findViewById(R.id.dateButton);
+
+        DatePickerDialog.OnDateSetListener date = (view, year, month, day) -> {
+
+            LocalDate actualDate = LocalDate.of(year, month, day);
+            long days = actualDate.until(currentDate, ChronoUnit.DAYS);
+            updateLabel(days);
+        };
+
+        button.setOnClickListener(view ->
+                new DatePickerDialog(MainActivity.this,
+                        date, currentDate.getYear(),
+                        currentDate.getMonthValue(),
+                        currentDate.getDayOfMonth()).show()
+        );
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+    }
+    private void updateLabel(long days){
+
+        relation_date.setText(getString(R.string.days, (days)));
     }
 
 }
